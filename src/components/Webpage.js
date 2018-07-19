fed0fc4ff37e3c6b3f6a450889a104ef9b5a7b46
Kelsey Fecho <kfecho30@gmail.com>
2018-07-19 16:39:58 +0000

    automatically backed up by learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,11 @@
+class Webpage extends React.Component{
+  render(){
+    return(
+      <div>
+        <title>The world's coolest webpage</title>
+        < FillerText />
+        < FillerText />
+      </div>
+    )
+  }
+}
